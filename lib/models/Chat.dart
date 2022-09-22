9715ea033c7bd25bccb7d126e0f722efc7e147fe
DatDM <datdm.work@gmail.com>
2022-09-22 23:47:12 +0700

    Update list chat

---
+++ lib/models/Chat.dart
@@ -0,0 +1,71 @@
+class Chat {
+  final String name, lastMessage, image, time;
+  final bool isActive;
+
+  Chat({
+    this.name = '',
+    this.lastMessage = '',
+    this.image = '',
+    this.time = '',
+    this.isActive = false,
+  });
+}
+
+List dataChats = [
+  Chat(
+    name: "Jenny Wilson",
+    lastMessage: "Hope you aree  doing well...",
+    image: "assets/img/user.png",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Esther Howard",
+    lastMessage: "Hello Abdullah! I am...",
+    image: "assets/img/user_2.png",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Ralph Edwards",
+    lastMessage: "Do you have update...",
+    image: "assets/img/user_3.png",
+    time: "5d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Jacob Jones",
+    lastMessage: "You’re welcome :)",
+    image: "assets/img/user_4.png",
+    time: "5d ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Albert Flores",
+    lastMessage: "Thanks",
+    image: "assets/img/user_5.png",
+    time: "6d ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Jenny Wilson",
+    lastMessage: "Hope you are doing well...",
+    image: "assets/img/user.png",
+    time: "3m ago",
+    isActive: false,
+  ),
+  Chat(
+    name: "Esther Howard",
+    lastMessage: "Hello Abdullah! I am...",
+    image: "assets/img/user_2.png",
+    time: "8m ago",
+    isActive: true,
+  ),
+  Chat(
+    name: "Ralph Edwards",
+    lastMessage: "Do you have update...",
+    image: "assets/img/user_3.png",
+    time: "5d ago",
+    isActive: false,
+  ),
+];
--- lib/screen/chats/components/body.dart
+++ lib/screen/chats/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:chat_messsaging/components/fill_outline_button.dart';
+import 'package:chat_messsaging/models/chat.dart';
 import 'package:chat_messsaging/shared/constants/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +8,78 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(defaultPadding, 0, defaultPadding, defaultPadding),
-      color: primrayColor,
+    return Column(
+      children: [
+         Container(
+          padding: const EdgeInsets.fromLTRB(defaultPadding, 0, defaultPadding, defaultPadding * 0.5),
+          color: primrayColor,
+          child: Row(
+            children: [
+              FillOutlineButton(text: "Recent Message", onPress: () {}),
+              const SizedBox(width: defaultPadding / 2),
+              FillOutlineButton(text: "Active", onPress: () {}, isFilled: false)
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: dataChats.length,
+            itemBuilder: (BuildContext contenxt, int index) => ChatCard(chat: dataChats[index]),
+          )
+        )
+      ],
+    );
+  }
+}
+
+class ChatCard extends StatelessWidget {
+  const ChatCard({
+    Key? key,
+    required this.chat,
+  }) : super(key: key);
+
+  final Chat chat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: defaultPadding * 0.75, horizontal: defaultPadding),
       child: Row(
         children: [
-          FillOutlineButton(text: "Recent Message", onPress: () {}),
-          const SizedBox(width: defaultPadding / 2),
-          FillOutlineButton(text: "Active", onPress: () {}, isFilled: false)
-        ],
+          CircleAvatar(
+            backgroundImage: AssetImage(chat.image),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    chat.name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15,
+                    )
+                  ),
+                  const SizedBox(height: 6),
+                  Opacity(
+                    opacity: 0.6,
+                    child: Text(
+                      chat.lastMessage,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ),
+          Opacity(
+            opacity: 0.6,
+            child: Text(chat.time)
+          )
+        ]
       ),
     );
   }
