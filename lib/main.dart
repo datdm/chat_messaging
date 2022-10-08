e6040f83d0c67c011be65e4c1316b0190547d13b
DatDM <datdm.work@gmail.com>
2022-10-09 00:00:43 +0700

    Update config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:chat_messsaging/screen/welcome/welcome_screen.dart';
+import 'package:chat_messsaging/shared/constants/firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: FirebaseConstants.apiKey,
+        appId: FirebaseConstants.appId,
+        messagingSenderId: FirebaseConstants.messagingSenderId,
+        projectId: FirebaseConstants.projectId,
+      )
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
---
+++ lib/shared/constants/firebase.dart
@@ -0,0 +1,6 @@
+class FirebaseConstants {
+  static String apiKey = "AIzaSyAMVSPugQiroYx-gFUFbtJuAHjiw51mX6E";
+  static String appId = "1:853918326702:web:6f837c57d7aa8968cbb23f";
+  static String messagingSenderId = "853918326702";
+  static String projectId = "chat-messaging-82389";
+}
